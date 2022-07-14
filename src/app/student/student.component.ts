f674a582a7d48e9e8cabfeac2efaa6c7f89c9333
billytrinh <quanghoa.trinh@gmail.com>
2022-07-14 11:18:32 +0700

    first commit

---
+++ src/app/student/student.component.ts
@@ -0,0 +1,10 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector:'app-student',
+  //template:'<h1>Day la component cua Student</h1><h2>Vi du h2</h2>'
+  templateUrl:'./student.component.html'
+})
+export class StudentComponent {
+
+}
