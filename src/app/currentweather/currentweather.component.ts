81161cab15f840b1f64076493fd34b68157c67bf
billytrinh <quanghoa.trinh@gmail.com>
2022-07-19 10:48:56 +0700

    call api with params - change city

--- src/app/currentweather/currentweather.component.ts
+++ src/app/currentweather/currentweather.component.ts
@@ -8,20 +8,24 @@ import {HttpClient,HttpParams} from "@angular/common/http";
 })
 export class CurrentweatherComponent {
     data: ICurrentweather | undefined;
-
+    cityCode = 'Hanoi';
     // tim cach lay du lieu tu api roi cho vao bien data
   constructor(private http: HttpClient) { //hàm này tự động chạy sau khi tạo component
   }
 
   ngOnInit(){ // hàm này tự động chạy sau khi print html xong
-      const url = 'https://api.openweathermap.org/data/2.5/weather';
-      let params = new HttpParams();
-      params =  params.append('q','Hanoi');
-      params =  params.append('appid','09a71427c59d38d6a34f89b47d75975c');
-      params =  params.append('units','metric');
-      this.http.get<ICurrentweather>(url,{params: params})
-        .subscribe(value=>{
-          this.data = value;
-        });
+      this.changeCity();
+  }
+
+  changeCity(){
+    const url = 'https://api.openweathermap.org/data/2.5/weather';
+    let params = new HttpParams();
+    params =  params.append('q',this.cityCode);
+    params =  params.append('appid','09a71427c59d38d6a34f89b47d75975c');
+    params =  params.append('units','metric');
+    this.http.get<ICurrentweather>(url,{params: params})
+      .subscribe(value=>{
+        this.data = value;
+      });
   }
 }
