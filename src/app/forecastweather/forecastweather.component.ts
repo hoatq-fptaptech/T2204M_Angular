a12c715ff33d34b683ec92fb7fdac9fc25f9634e
billytrinh <quanghoa.trinh@gmail.com>
2022-07-21 08:34:48 +0700

    demo forecast - params

--- src/app/forecastweather/forecastweather.component.ts
+++ src/app/forecastweather/forecastweather.component.ts
@@ -1,6 +1,6 @@
 import {Component} from "@angular/core";
 import {IForecast} from "../interfaces/forecast.interface";
-import {HttpClient} from "@angular/common/http";
+import {HttpClient,HttpParams} from "@angular/common/http";
 
 @Component({
   selector: 'app-forecast',
@@ -13,7 +13,11 @@ export class ForecastweatherComponent {
   }
 
   ngOnInit(){
-    const url = 'http://api.openweathermap.org/data/2.5/forecast?q=Hanoi,vietnam&appid=09a71427c59d38d6a34f89b47d75975c&units=metric';
+    const url = 'http://api.openweathermap.org/data/2.5/forecast';
+    let params = new HttpParams();
+    params = params.append("q",'Hanoi');
+    params = params.append("appid",'09a71427c59d38d6a34f89b47d75975c');
+    params = params.append("units",'metric');
     this.http.get<IForecast>(url)
       .subscribe(value=>{
         this.data = value;
